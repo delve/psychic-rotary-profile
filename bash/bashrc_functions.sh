5614ee2c73cc219c5b1d58c1ee15937eb034b9d7
Grady Brandt <delve@users.noreply.github.com>
2026-05-26 09:35:19 +0100

    init

---
+++ bash/bashrc_functions.sh
@@ -0,0 +1,21 @@
+. ~/k8s_functions.sh
+
+### Python venv functions
+function venv {
+    if [ -d ".venv" ]; then
+        source .venv/bin/activate
+    else
+        echo "No virtual environment found in the current directory."
+    fi
+}
+
+function newVenv {
+    if [ -d ".venv" ]; then
+        echo ".venv already exists. Activating it."
+        source .venv/bin/activate
+    else
+        python3 -m venv .venv
+        source .venv/bin/activate
+    fi
+}
+### end Python venv functions
